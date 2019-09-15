ec628bccd9054da8a84bfe4b2c20cdf1e830fc40
irwin <irgranados16@gmail.com>
2019-09-15 16:28:09 -0500

    install and set up browsersync with gulp for server and livereload

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: ["./", "./pages/", "./css/", "./js/" ]
+    });
+
+    gulp.watch("./pages/*.html").on('change', browserSync.reload);
+    gulp.watch("./css/*.css").on('change', browserSync.reload);
+    gulp.watch("./js/*.js").on('change', browserSync.reload);
+});
+
+
+
+gulp.task('default', gulp.series('browser-sync'));
