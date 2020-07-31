154d905f2e573fc7499c99f650d911ab9330e9e2
Irwin <irgranados16@gmail.com>
2020-07-30 23:00:01 -0500

    create search box and js logic to store box value

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,10 @@
-console.log('Ready')
+var submitButton = $('#submitNumber');
+var numberInput = $('#numberInput');
+var input;
+
+
+submitButton.on('click', function (e) {
+	e.preventDefault();
+	input = parseInt(numberInput[0].value);
+	console.log(input);
+});
