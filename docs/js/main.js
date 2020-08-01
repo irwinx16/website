89f9188f60e982db4718ad28830019a5dbd50625
Irwin <irgranados16@gmail.com>
2020-08-01 12:31:40 -0500

    add ajax call code and replace jquery cdn script to full version

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,18 @@
-var submitButton = $('#submitNumber');
-var numberInput = $('#numberInput');
-var input;
-
-
-submitButton.on('click', function (e) {
+console.log('READY');
+$('#submitNumber').on('click', function (e) {
 	e.preventDefault();
-	input = parseInt(numberInput[0].value);
-	console.log(input);
+	// grab text from input box
+	var text = $('#numberInput').val();
+
+	// Construct the URL
+	var queryURL = "https://www.omdbapi.com/?t=" + text + "&apikey=trilogy";
+
+	// jQuery Call
+
+	$.ajax({
+		url: queryURL,
+		method: 'GET'
+	}).then(function (response) {
+		$('#ajaxRes').text(JSON.stringify(response));
+	});
 });
