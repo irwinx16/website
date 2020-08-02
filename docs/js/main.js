4015f134cf91b4d3c140f4f3560d78d4f72eeb36
Irwin <irgranados16@gmail.com>
2020-08-02 16:55:53 -0500

    create love page and add ajax call code

--- docs/js/main.js
+++ docs/js/main.js
@@ -16,3 +16,51 @@ $('#submitNumber').on('click', function (e) {
 		$('#ajaxRes').text(JSON.stringify(response));
 	});
 });
+
+// Love Calculator Page
+$('#submitTest').on('click', function (e) {
+	e.preventDefault();
+	var access_auth = "d729914ef4msh2509ea0599aca52p1d7882jsn76301ff4760b"
+	var name1 = $('#name1').val();
+	var name2 = $('#name2').val();
+
+	var url = "https://love-calculator.p.rapidapi.com/getPercentage?fname=" + name1 + "&sname=" + name2 + "&apiKey=" + access_auth;
+
+	// Ajax Call
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": url,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "love-calculator.p.rapidapi.com",
+			"x-rapidapi-key": access_auth
+		}
+	}
+
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+
+		$('#loveResult').empty();
+		// create div
+		var resDiv = $("<div class='results'> ");
+		// store data
+		var resName1 = response.fname;
+		var resName2 = response.sname;
+		var resPrcnt = response.percentage;
+		var resResul = response.result;
+
+		// create and append element with the data
+		var namesEl = $("<p class='text-center'>").text(resName1 + ' and ' + resName2);
+		resDiv.append(namesEl);
+
+		var compatibleEL = $("<p class='text-center'>").text('are ' + resPrcnt + ' % compatible');
+		resDiv.append(compatibleEL);
+
+		var resultEL = $("<p class='text-center'>").text(resResul);
+		resDiv.append(resultEL);
+
+		$('#loveResult').prepend(resDiv);
+	});
+
+});
